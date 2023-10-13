4330654872d7a81894cc389bacf12fae446ec1ed
Isaac <isaacfunes@icloud.com>
2023-10-13 11:05:15 -0500

    Added classes to WhatGPT3 JSX file on divs

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -6,16 +6,16 @@ const WhatGPT3 = () => {
   return (
     <div className="gpt3__whatgpt3 section__margin" id='whpt3'>
       <div className="gpt3__whatgpt3-feature">
-        <Feature />
+        <Feature title='What is GPT-3' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, asperiores quisquam. Dolor, vel natus ex asperiores delectus veniam odit tempora officia. Magnam rerum, mollitia delectus eum quaerat tempore nesciunt esse!'/>
       </div>
       <div className='gpt3__whatgpt3-heading'>
         <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
         <p>Explore The Library</p>
       </div>
       <div className="gpt3__whatgpt3-container">
-        <Feature />
-        <Feature />
-        <Feature />
+        <Feature title='Chatbots' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas quis, unde a ullam praesentium beatae corporis.'/>
+        <Feature title='Knowledgebase' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis culpa facilis dolores itaque praesentium officia eum saepe quaerat accusantium. Saepe?'/>
+        <Feature title='Education' text='Lorem ipsum dolor sit, amet consectetur adipisicing elit. Debitis culpa facilis dolores itaque praesentium officia eum saepe quaerat accusantium. Saepe?'/>
       </div>
     </div>
   )
