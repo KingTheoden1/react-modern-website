565b98e756a6a4f5a20ce3d5837eaead872d3825
Isaac <isaacfunes@icloud.com>
2023-10-16 14:02:58 -0500

    Finished responsive design and content styling in CSS file, section complete

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -5,7 +5,7 @@ const CTA = () => {
   return (
     <div className='gpt3__cta'>
         <div className="gpt3__cta-content">
-          <p>Request Early Access</p>
+          <p>Request Early Access to Get Started</p>
           <h3>Register today & start exploring the endless possibilities</h3>
         </div>
         <div className="gpt3__cta-btn">
