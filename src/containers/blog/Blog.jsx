344d80dbbba9715087839c0d002759b63853d09d
Isaac <isaacfunes@icloud.com>
2023-10-16 15:21:12 -0500

    Set up import.js file for articles

--- src/containers/blog/Blog.jsx
+++ src/containers/blog/Blog.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Article } from '../../components';
+import { blog01, blog02, blog03, blog04, blog05 } from './imports';
 import './blog.css';
 
 const Blog = () => {
@@ -10,13 +11,13 @@ const Blog = () => {
       </div>
       <div className="gpt3__container">
         <div className="gpt3__blog-container_groupA">
-          <Article />
+          <Article imgUrl={blog01} date='Oct 16, 2023' title='GPT-3 and OpenAI is the future. Let us explore how that us.'/>
         </div>
         <div className="gpt3__blog-container_groupB">
-          <Article />
-          <Article />
-          <Article />
-          <Article />
+          <Article imgUrl={blog02} date='Oct 16, 2023' title='GPT-3 and OpenAI is the future. Let us explore how that us.'/>
+          <Article imgUrl={blog03} date='Oct 16, 2023' title='GPT-3 and OpenAI is the future. Let us explore how that us.'/>
+          <Article imgUrl={blog04} date='Oct 16, 2023' title='GPT-3 and OpenAI is the future. Let us explore how that us.'/>
+          <Article imgUrl={blog05} date='Oct 16, 2023' title='GPT-3 and OpenAI is the future. Let us explore how that us.'/>
         </div>
       </div>
     </div>
