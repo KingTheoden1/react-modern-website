2d74649525cdb6eda7c319daf5189d8c9644fda7
Isaac <isaacfunes@icloud.com>
2023-10-16 13:37:22 -0500

    Finished Possibilities section done

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -10,7 +10,7 @@ const Possibility = () => {
         </div>
         <div className="gpt3__possibility-content">
           <h4>Request Early Access to Get Started</h4>
-          <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+          <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
           <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium dolor cum error facere rem recusandae sit fugit dicta alias facilis laudantium quibusdam repudiandae dolorem, sunt, vel temporibus aperiam.</p>
           <h4>Request Early Access to Get Started</h4>
         </div>
@@ -18,4 +18,4 @@ const Possibility = () => {
   )
 }
 
-export default Possibility
+export default Possibility;
