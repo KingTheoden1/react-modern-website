4d96ae0b3aa6e18c76596dbd0d25161a8b6b112c
Isaac <isaacfunes@icloud.com>
2023-10-16 13:24:17 -0500

    Finished Possibility content and components

--- src/containers/possibility/Possibility.jsx
+++ src/containers/possibility/Possibility.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import PossibilityImage from '../../assets/possibility.png';
 import './possibility.css';
 
 const Possibility = () => {
   return (
-    <div>
-        Possibility
+    <div className='gpt3__possibility section__padding' id='possibility'>
+        <div className="gpt3__possibility-image">
+          <img src={PossibilityImage} alt="possibility" />
+        </div>
+        <div className="gpt3__possibility-content">
+          <h4>Request Early Access to Get Started</h4>
+          <h1 className="gradient-text">The possibilities are beyond your imagination</h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Accusantium dolor cum error facere rem recusandae sit fugit dicta alias facilis laudantium quibusdam repudiandae dolorem, sunt, vel temporibus aperiam.</p>
+          <h4>Request Early Access to Get Started</h4>
+        </div>
     </div>
   )
 }
--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -4,7 +4,7 @@ import { Feature } from '../../components';
 
 const WhatGPT3 = () => {
   return (
-    <div className="gpt3__whatgpt3 section__margin" id='whpt3'>
+    <div className="gpt3__whatgpt3 section__margin" id='wgpt3'>
       <div className="gpt3__whatgpt3-feature">
         <Feature title='What is GPT-3' text='Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim, asperiores quisquam. Dolor, vel natus ex asperiores delectus veniam odit tempora officia. Magnam rerum, mollitia delectus eum quaerat tempore nesciunt esse!'/>
       </div>
